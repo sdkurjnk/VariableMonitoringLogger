82b2527baf89dd6f33228cc85bc4fbfff4b5eaaf
sdkurjnk <sdkurjnk@naver.com>
2026-02-23 15:07:40 +0900

    Privating instance variable

--- src/vml/logger.py
+++ src/vml/logger.py
@@ -8,33 +8,33 @@ class vml:
     history = []
 
     def __init__(self, var_names, filename="log.jsonl"):
-        self.filename = filename
-        self.var_names = var_names
-        self.last_var_ref = None
-        self.last_var_copy = None
-        self.domain_int = None
-        self._active = True
-        self._tracing_internal = False
+        self.__filename = filename
+        self.__var_names = var_names
+        self.__last_var_ref = None
+        self.__last_var_copy = None
+        self.__domain_int = None
+        self.__active = True
+        self.__tracing_internal = False
 
         target_frame = sys._getframe(1)
         self.target_frame = target_frame
         
-        if self.var_names in target_frame.f_locals:
-            target_var = target_frame.f_locals.get(self.var_names)
-            self.domain_int = 0 #Local
-        elif self.var_names in target_frame.f_globals:
-            target_var = target_frame.f_globals.get(self.var_names)
-            self.domain_int = 1 #Global
+        if self.__var_names in target_frame.f_locals:
+            target_var = target_frame.f_locals.get(self.__var_names)
+            self.__domain_int = 0 #Local
+        elif self.__var_names in target_frame.f_globals:
+            target_var = target_frame.f_globals.get(self.__var_names)
+            self.__domain_int = 1 #Global
         else:
-            print(f"{self.var_names} is not found.")
+            print(f"{self.__var_names} is not found.")
             sys.exit(0)
 
-        self.last_var_ref = target_var
-        self.last_var_copy = copy.deepcopy(target_var)
+        self.__last_var_ref = target_var
+        self.__last_var_copy = copy.deepcopy(target_var)
         
         vml.history.append({
-            "name" : self.var_names,
-            "data" : self.last_var_copy,
+            "name" : self.__var_names,
+            "data" : self.__last_var_copy,
             "event" : "init"
         })
 
@@ -47,56 +47,54 @@ class vml:
         return self._trace_lines
 
     def _trace_lines(self, frame, event, arg):
-        if not self._active or event != 'line':
+        if not self.__active or event != 'line' or self.__tracing_internal:
             return self._trace_lines
+        
+        self.__tracing_internal = True
 
-        if self._tracing_internal:
-            return self._trace_lines
-        self._tracing_internal = True
-
-        if self.domain_int == 0:
+        if self.__domain_int == 0: #Local
             if frame is not self.target_frame:
-                self._tracing_internal = False
+                self.__tracing_internal = False
                 return self._trace_lines
-        else:
+        else: #Global
             if frame.f_code.co_filename == __file__:
-                self._tracing_internal = False
+                self.__tracing_internal = False
                 return self._trace_lines
 
         result = vml_engine.check_variable(
             frame, 
-            self.last_var_ref, 
-            self.last_var_copy, 
-            self.domain_int, 
-            self.var_names
+            self.__last_var_ref, 
+            self.__last_var_copy, 
+            self.__domain_int, 
+            self.__var_names
         )
 
-        if result == 1:
-            current_var = frame.f_locals.get(self.var_names) if self.domain_int == 0 else frame.f_globals.get(self.var_names)
-            self.last_var_ref = current_var
-            self.last_var_copy = copy.deepcopy(current_var)
+        if result == 1: #When the variable is updated
+            current_var = frame.f_locals.get(self.__var_names) if self.__domain_int == 0 else frame.f_globals.get(self.__var_names)
+            self.__last_var_ref = current_var
+            self.__last_var_copy = copy.deepcopy(current_var)
             vml.history.append({
-                "name" : self.var_names,
-                "data" : self.last_var_copy,
+                "name" : self.__var_names,
+                "data" : self.__last_var_copy,
                 "event" : "updated"
             })
-        elif result is None:
+        elif result is None: #When the variable is deleted
             vml.history.append({
-                "name" : self.var_names,
+                "name" : self.__var_names,
                 "data" : None,
                 "event" : "deleted"
             })
-            self.last_var_ref = None
-            self.last_var_copy = None
-            self._active = False
+            self.__last_var_ref = None
+            self.__last_var_copy = None
+            self.__active = False
         
-        self._tracing_internal = False
+        self.__tracing_internal = False
         return self._trace_lines
 
     def _final_save(self):
         sys.settrace(None)
         if not vml.history: return
-        with open(self.filename, "w", encoding="utf-8") as f:
+        with open(self.__filename, "w", encoding="utf-8") as f:
             for entry in vml.history:
                 f.write(json.dumps(entry, ensure_ascii=False) + "\n")
         vml.history.clear()
