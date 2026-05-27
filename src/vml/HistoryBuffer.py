ebcd50e5a5060338a9fc7e5bb65621c46ac456f3
dogyabob <lmjksk0514@gmail.com>
2026-05-27 21:23:25 +0900

    feat: add line, domain to json

--- src/vml/HistoryBuffer.py
+++ src/vml/HistoryBuffer.py
@@ -2,8 +2,10 @@ class HistoryBuffer:
     def __init__(self):
         self._history = []
 
-    def append(self, name, data, event):
-        self._history.append({"name" : name, "data" : data, "event" : event})
+    _DOMAIN_LABELS = {0: "LOCAL", 1: "GLOBAL"}
+
+    def append(self, name, data, event, domain, line):
+        self._history.append({"name" : name, "data" : data, "event" : event, "domain" : self._DOMAIN_LABELS.get(domain, domain), "line" : line})
 
     def getHistory(self):
         return self._history.copy()
