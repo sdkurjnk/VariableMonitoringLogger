4ce41b43d70371938829b07ff1194f308343fecb
dogyabob <lmjksk0514@gmail.com>
2026-05-27 23:23:49 +0900

    fix: add label -1 : UNKNOWN

--- src/vml/HistoryBuffer.py
+++ src/vml/HistoryBuffer.py
@@ -5,9 +5,9 @@ class HistoryBuffer:
         self._history = []
 
     _DOMAIN_LABELS = {0: "LOCAL", 1: "GLOBAL"}
-
+    _UNKOWN_DOMAIN_LABEL = "UNKNOWN"
     def append(self, name, data, event, domain=None, line=None):
-        self._history.append({"name" : name, "data" : data, "event" : event, "domain" : self._DOMAIN_LABELS.get(domain, domain), "line" : line})
+        self._history.append({"name" : name, "data" : data, "event" : event, "domain" : self._DOMAIN_LABELS.get(domain, self._UNKOWN_DOMAIN_LABEL), "line" : line})
 
     def getHistory(self):
         return copy.deepcopy(self._history)
