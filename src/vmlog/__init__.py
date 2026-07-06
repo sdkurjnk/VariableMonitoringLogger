c63eecc5008493f96448c878c9b297d82547de84
dogyabob <lmjksk0514@gmail.com>
2026-07-06 20:18:29 +0900

    refactor : expose VMlog as a module-level singleton

--- src/vmlog/__init__.py
+++ src/vmlog/__init__.py
@@ -1,11 +1,26 @@
+import sys
+import threading
 from importlib.metadata import PackageNotFoundError, version
 
 from . import vmlog_engine
-from .vmlog import VMlog, logger
+from .vmlog import _vmlog
 
 try:
     __version__ = version("vmlog")
 except PackageNotFoundError:
     __version__ = "unknown"
 
-__all__ = ["VMlog", "logger", "vmlog_engine"]
+__all__ = ["logRegister"]
+
+_instance = None
+_instance_lock = threading.Lock()
+
+def logRegister(varName):
+    global _intance
+
+    if _intance is None:
+        with _instance_lock:
+            if _intance is None:
+                _instance = _vmlog()
+
+    _instance.register(varName, sys._getframe(1))
